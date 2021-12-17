fb41d89f1ce1e0a785ce7c63878c43e4842f511f
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-12-17 20:48:57 +0100

    Spec that Solidus is loading on homepage

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -2,8 +2,7 @@ require "system_helper"
 
 RSpec.describe "Static pages" do
   it "visiting index" do
-    visit root_path
-    expect(page).to have_content("Welcome")
-    expect(page).to have_content("Hello World From StimulusController!")
+    visit "/"
+    expect(page).to have_content("Solidus")
   end
 end
